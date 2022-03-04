3b5f917a735c77f3fa399a8d26625c9ef4037d19
AndreiTotoro <AndreiTotoro@gmail.com>
2022-03-04 16:52:12 +0200

    Completley implemented the choice system

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-console.log("test");
 
 const computerPlay = () =>{
-    return Math.floor(Math.random() * 3)
+    const choice = Math.floor(Math.random() * 3)
+    if(choice == 0){
+        return "rock"
+    }
+    if(choice == 1){
+        return "paper"
+    }
+    else{
+        return "scissors"
+    }
+}
+
+const playerSelection = () =>{
+    return prompt("Rock, Paper, Scissors?").toLowerCase()
+}
+
+const playRound = (computerPlay, playerSelection) => {
+    console.log(playerSelection + " " + computerPlay)
 }
