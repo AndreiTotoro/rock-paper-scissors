13f258539965f7c9b454a0904cf067868e84b6ab
AndreiTotoro <AndreiTotoro@gmail.com>
2022-03-04 17:22:37 +0200

    Added the ability for users to play games

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const computerPlay = () =>{
+const computerSelection = () =>{
     const choice = Math.floor(Math.random() * 3)
     
     if(choice == 0){
@@ -21,34 +21,73 @@ const playerSelection = () =>{
     return choice
 }
 
-const playRound = (computerPlay, playerSelection) => {
-    if(playerSelection == "rock" && computerPlay == "rock"){
+const playRound = (computerSelection, playerSelection) => {
+    if(playerSelection == "rock" && computerSelection == "rock"){
         alert("Computer played rock. It's a tie!")
+        return 2
     }
-    if(playerSelection == "rock" && computerPlay == "paper"){
+    if(playerSelection == "rock" && computerSelection == "paper"){
         alert("Computer played paper. You lost!")
+        return 0
     }
-    if(playerSelection == "rock" && computerPlay == "scissors"){
+    if(playerSelection == "rock" && computerSelection == "scissors"){
         alert("Computer played scissors. You won!")
+        return 1
     }
 
-    if(playerSelection == "paper" && computerPlay == "rock"){
+    if(playerSelection == "paper" && computerSelection == "rock"){
         alert("Computer played rock. You won!")
+        return 1
     }
-    if(playerSelection == "paper" && computerPlay == "paper"){
+    if(playerSelection == "paper" && computerSelection == "paper"){
         alert("Computer played paper. It's a tie!")
+        return 2
     }
-    if(playerSelection == "paper" && computerPlay == "scissors"){
+    if(playerSelection == "paper" && computerSelection == "scissors"){
         alert("Computer played scissors. You lost!")
+        return 0
     }
 
-    if(playerSelection == "scissors" && computerPlay == "rock"){
+    if(playerSelection == "scissors" && computerSelection == "rock"){
         alert("Computer played rock. You lost!")
+        return 0
     }
-    if(playerSelection == "scissors" && computerPlay == "paper"){
+    if(playerSelection == "scissors" && computerSelection == "paper"){
         alert("Computer played paper. You won!")
+        return 1
     }
-    if(playerSelection == "scissors" && computerPlay == "scissors"){
+    if(playerSelection == "scissors" && computerSelection == "scissors"){
         alert("Computer played scissors. It's a tie!")
+        return 2
     }
 }
+
+const game = () =>{
+    const name = prompt("What's your name?")
+    let pscore = 0;
+    let cscore = 0;
+    for(let i = 1; i <= 5; i++){
+        const result = playRound(playerSelection(), computerSelection())
+        if (result == 0){
+            cscore++;
+        }
+        if(result == 1){
+            pscore++;
+        } else{
+            cscore++;
+            pscore++;
+        }
+    }
+    if(pscore > cscore){
+        alert(`The score is ${pscore} to ${cscore}! ${name} has won!`)
+    }
+    if(cscore > pscore){
+        alert(`The score is ${cscore} to ${pscore}! ${name} has lost!`)
+    }
+    if(cscore == pscore)
+    {
+        alert(`The score is ${cscore} to ${pscore}! It's a tie!`)
+    }
+}
+
+game()
