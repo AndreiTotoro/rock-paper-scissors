526db2effb96e7b66636b5152e23a4a397531b47
AndreiTotoro <AndreiTotoro@gmail.com>
2022-05-27 13:37:18 +0300

    Made buttons that work

--- main.js
+++ main.js
@@ -1,68 +1,77 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
 
-const computerSelection = () =>{
-    const choice = Math.floor(Math.random() * 3)
-    
-    if(choice == 0){
-        return "rock"
-    }
-    if(choice == 1){
-        return "paper"
-    }
-    else{
-        return "scissors"
-    }
-}
+const computerSelection = () => {
+	const choice = Math.floor(Math.random() * 3);
 
-const playerSelection = () =>{
-    let choice = prompt("Rock, Paper, Scissors?").toLowerCase()
-    while(choice != "rock" && choice != "paper" && choice != "scissors"){
-        choice = prompt("Invalid Option! Please choose between: Rock, Paper and Scissors.")
-    } 
-    return choice
-}
+	if (choice == 0) {
+		return 'rock';
+	}
+	if (choice == 1) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+};
+
+const playerSelection = () => {
+	let choice = prompt('Rock, Paper, Scissors?').toLowerCase();
+	while (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
+		choice = prompt(
+			'Invalid Option! Please choose between: Rock, Paper and Scissors.'
+		);
+	}
+	return choice;
+};
 
 const playRound = (computerSelection, playerSelection) => {
-    if(playerSelection == "rock" && computerSelection == "rock"){
-        alert("Computer played rock. It's a tie!")
-        return 2
-    }
-    if(playerSelection == "rock" && computerSelection == "paper"){
-        alert("Computer played paper. You lost!")
-        return 0
-    }
-    if(playerSelection == "rock" && computerSelection == "scissors"){
-        alert("Computer played scissors. You won!")
-        return 1
-    }
+	if (playerSelection == 'rock' && computerSelection == 'rock') {
+		alert("Computer played rock. It's a tie!");
+		return 2;
+	}
+	if (playerSelection == 'rock' && computerSelection == 'paper') {
+		alert('Computer played paper. You lost!');
+		return 0;
+	}
+	if (playerSelection == 'rock' && computerSelection == 'scissors') {
+		alert('Computer played scissors. You won!');
+		return 1;
+	}
 
-    if(playerSelection == "paper" && computerSelection == "rock"){
-        alert("Computer played rock. You won!")
-        return 1
-    }
-    if(playerSelection == "paper" && computerSelection == "paper"){
-        alert("Computer played paper. It's a tie!")
-        return 2
-    }
-    if(playerSelection == "paper" && computerSelection == "scissors"){
-        alert("Computer played scissors. You lost!")
-        return 0
-    }
+	if (playerSelection == 'paper' && computerSelection == 'rock') {
+		alert('Computer played rock. You won!');
+		return 1;
+	}
+	if (playerSelection == 'paper' && computerSelection == 'paper') {
+		alert("Computer played paper. It's a tie!");
+		return 2;
+	}
+	if (playerSelection == 'paper' && computerSelection == 'scissors') {
+		alert('Computer played scissors. You lost!');
+		return 0;
+	}
 
-    if(playerSelection == "scissors" && computerSelection == "rock"){
-        alert("Computer played rock. You lost!")
-        return 0
-    }
-    if(playerSelection == "scissors" && computerSelection == "paper"){
-        alert("Computer played paper. You won!")
-        return 1
-    }
-    if(playerSelection == "scissors" && computerSelection == "scissors"){
-        alert("Computer played scissors. It's a tie!")
-        return 2
-    }
-}
+	if (playerSelection == 'scissors' && computerSelection == 'rock') {
+		alert('Computer played rock. You lost!');
+		return 0;
+	}
+	if (playerSelection == 'scissors' && computerSelection == 'paper') {
+		alert('Computer played paper. You won!');
+		return 1;
+	}
+	if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+		alert("Computer played scissors. It's a tie!");
+		return 2;
+	}
+};
 
-const game = () =>{
+buttons.forEach((button) => {
+	button.addEventListener('click', () => {
+		playRound(computerSelection(), button.innerHTML);
+	});
+});
+
+/*const game = () =>{
     const name = prompt("What's your name?")
     let playerScore = 0;
     let computerScore = 0;
@@ -91,3 +100,4 @@ const game = () =>{
 }
 
 game()
+*/
